48045f64db75b6325da755bbffc3fbdf08639548
Lutz Mitländer <lutzmitlaender@gmx.net>
2025-05-12 00:58:58 +0200

    pre-monday commit

--- try_mitl.bat
+++ try_mitl.bat
@@ -1,4 +1,4 @@
 javac -cp .;geo.jar mitl/*.java
 jar -cf mitl.jar -C . mitl
-java -cp geo.jar;mitl.jar mitl.Mapout 49.479964 10.988507 4096 2048 2000.0 mapout.png
+java -cp geo.jar;mitl.jar mitl.Mapout 49.479964 10.988507 4096 2048 400.0 mapout.png
 start "" "mapout.png"
